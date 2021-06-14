93f1790278715008a821b218a3c65d84d197740b
Lautaro Lopez <40278720+LautaroLopez01@users.noreply.github.com>
2021-06-14 13:16:12 -0300

    actualizacion cooperativa

--- sprint1/Codigo/src/Cooperativa.java
+++ sprint1/Codigo/src/Cooperativa.java
@@ -1,17 +1,33 @@
 import java.util.ArrayList;
 
 public class Cooperativa {
-	
-	//ArrayList<>
-	
-	private ArrayList<SolicitudRetiro> solicitudes; 
-	private CarteleraVirtual cartelera ; 
-	
-	
-	String Secretaria, password ; //Referente a usuario y contraseña de secretaria
-	
+
+	// ArrayList<>
+
+	private ArrayList<SolicitudRetiro> solicitudes;
+	private CarteleraVirtual cartelera;
+
+	private String Secretaria, password; // Referente a usuario y contraseÃ±a de secretaria
+	private float x, y;
+
 	public void agregarSolicitud(SolicitudRetiro r) {
-		solicitudes.add(r) ; 
+		solicitudes.add(r);
 	}
-	
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
 }
