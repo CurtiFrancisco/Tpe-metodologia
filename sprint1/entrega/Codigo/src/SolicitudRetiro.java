09409602b739000400b97c7e91baa5653f48aa17
TomasElordi <82533461+TomasElordi@users.noreply.github.com>
2021-06-14 14:48:02 -0300

    Update SolicitudRetiro.java

--- sprint1/entrega/Codigo/src/SolicitudRetiro.java
+++ sprint1/entrega/Codigo/src/SolicitudRetiro.java
@@ -13,7 +13,33 @@ public class SolicitudRetiro {
 		this.opcionRetiro = opcionRetiro;
 	} 
 	
+	public int getFHorariaInicio(){
+		return this.fHorariaInicio;
+	}
 	
+	public int getFHorariaFinal(){
+		return this.fHorariaFinal;
+	}
+	
+	public int getOpcionRetiro(){
+		return this.OpcionRetiro;
+	}
+	
+	public void setFHorariaInicio(int fHorarioInicio){
+		this.fHorariaInicio = fHorariaInicio;
+	}
+	
+	public void setFHorariaFinal(int fHorarioFinal){
+		this.fHorariaFinal = fHorariaFinal;
+	}
+	
+	public void setOpcionRetiro(int opcionRetiro){
+		this.opcionRetiro = opcionRetiro;
+	}
+	
+	public int getCiudadano(){
+		return new Ciudadano(c.getNombre().c.getApellido(),c.getTelofono(),c.getDireccion_x(),c.getDireccion_y(),c.getCapacidad_Transporte());
+	}
 	
 	
 }
