2f96b92b0200368589c3d09cf4260c62c84b61f6
Francisco Curti <facurti@gmail.com>
2021-06-28 16:47:23 -0300

    hola

--- sprint2/Entrega/Codigo/src/Cooperativa/InterfazVisual.java
+++ sprint2/Entrega/Codigo/src/Cooperativa/InterfazVisual.java
@@ -1,91 +1,68 @@
 package Cooperativa;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import Roles.Secretaria;
 
 public class InterfazVisual {
 
-	public static void agregarCartonero(Cooperativa c) {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ; 
-		String nombre = " ";
-		String apellido = " " ; 
-		String direccion = " " ; 
-		String dni = " " ;
-		String ct = " " ;  
+	public static String ingresarDato() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String aux = " ";
 		try {
-			nombre = br.readLine();
-			apellido = br.readLine() ; 
-			direccion = br.readLine() ; 
-			dni = br.readLine() ; 
-			ct = br.readLine() ; 
+			aux = br.readLine();
 		} catch (IOException e) {
-			e.printStackTrace();  
-		} 
-		int capacidadTransporte = Integer.parseInt(ct) ;
-		c.agregarCartonero(nombre, apellido, direccion, dni, capacidadTransporte);
-		
+			e.printStackTrace();
+		}
+
+		return aux;
 	}
-	
+
+	public static void agregarCartonero(Cooperativa c) {
+		System.out.println("Ingrese el nombre: ");
+		String nombre = ingresarDato();
+		System.out.println("Ingrese el apellido: ");
+		String apellido = ingresarDato();
+		System.out.println("Ingrese la direccion: ");
+		String direccion = ingresarDato();
+		System.out.println("Ingrese el dni: ");
+		String dni = ingresarDato();
+		System.out.println("Ingrese la capacidad de transporte: ");
+		String ct = ingresarDato();
+		int capacidadTransporte = Integer.parseInt(ct);
+		c.agregarCartonero(nombre, apellido, direccion, dni, capacidadTransporte);
+
+	}
+
 	public static void eliminarCartonero(Cooperativa cooperativa) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ; 
-		String dni = " " ;
-		try { 
-			dni = br.readLine() ; 
-		} catch (IOException e) {
-			e.printStackTrace();  
-		} 
+		String dni = ingresarDato();
 		cooperativa.eliminarCartonero(dni);
 	}
-	
+
 	public static void agregarMaterial(Cooperativa c) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ;
-		String nombre = " ";
-		String descripcion = " " ;
-		String cantidad = " " ; 
-		try {
-			nombre = br.readLine();
-			descripcion = br.readLine() ;
-			cantidad = br.readLine() ; 
-		} catch (IOException e) {
-			e.printStackTrace();  
-		} 
-		int cant = Integer.parseInt(cantidad) ; 
-		c.agregarMaterial(new Material(nombre,descripcion), cant);
+		System.out.println("Ingrese el nombre: ");
+		String nombre = ingresarDato() ; 
+		System.out.println("Ingrese el descripcion: ");
+		String descripcion = ingresarDato() ; 
+		System.out.println("Ingrese el Cantidad: ");
+		String cantidad = ingresarDato();
+		int cant = Integer.parseInt(cantidad);
+		c.agregarMaterial(new Material(nombre, descripcion), cant);
 	}
 
 	public static void borrarMaterial(Cooperativa c) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ;
-		String nombre = " ";
-		String descripcion = " " ; 
-		try {
-			nombre = br.readLine();
-			descripcion = br.readLine() ; 
-		} catch (IOException e) {
-			e.printStackTrace();  
-		}  
-		c.borrarMaterial(new Material(nombre,descripcion));
-	}
-	
-	
-	
-	
-	
-	public static void main (String[] args) {
-		Cooperativa c = new Cooperativa() ; 
-		
-		c.menu(1);
-		
-		
+		String nombre = ingresarDato() ; 
+		String descripcion = ingresarDato() ;
+		c.borrarMaterial(new Material(nombre, descripcion));
 	}
 
-	
+	public static void main(String[] args) {
+		Cooperativa c = new Cooperativa();
+		Secretaria s = new Secretaria("Maria") ; 
+		s.menu(c);
 
+	}
 
-
-	
-	
 }
